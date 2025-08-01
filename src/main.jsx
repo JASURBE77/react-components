7669ceebaf6af21d19a11fe352f3b526301949d8
JASURBE77 <animesila89@gmail.com>
2025-08-01 14:44:32 +0500

    sdef

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Header from './components/Header'
-import Section1 from './components/Section1'
-import Section2 from './components/Section2'
-import Section3 from './components/Section3'
-import Section4 from './components/Section4'
-import Footer from './components/Footer'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-          <Header />
-          <main>
-            <Section1 /> 
-            <Section2 />
-            <Section3 />
-            <Section4 />
-          </main>
-            <Footer />
   </StrictMode>,
 )
