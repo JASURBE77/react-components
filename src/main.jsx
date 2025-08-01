30024409f3eff9f02351316c9a5030a86310abb2
JASURBE77 <animesila89@gmail.com>
2025-08-01 14:36:43 +0500

    jaska jonka

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import Header from './components/Header'
 import Section1 from './components/Section1'
 import Section2 from './components/Section2'
